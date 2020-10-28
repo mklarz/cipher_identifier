0726d2c8b8c3171674e48b28c7d75aea2cf0bd48
mklarz <mklarz@protonmail.com>
2020-10-28 14:02:19 +0100

    Use correct URL for the ciphers in CIPHERS.md

--- generate_ciphers_md.py
+++ generate_ciphers_md.py
@@ -7,7 +7,7 @@ import urllib.parse
 
 md_text = "# Cipher list\n"
 
-BASE_URL = "https://www.dcode.fr/ancients-stargate-alphabet"
+BASE_URL = "https://www.dcode.fr"
 BASE_PATH = pathlib.Path(__file__).parent.absolute()
 CIPHERS_PATH = "{}/ciphers".format(BASE_PATH)
 CIPHERS = sorted(next(os.walk(CIPHERS_PATH))[1])
