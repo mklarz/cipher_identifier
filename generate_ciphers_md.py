be8eb9fb39c33468de98fdc19cf8f2ce54a6396c
mklarz <mklarz@protonmail.com>
2020-10-28 20:17:37 +0100

    Update generate_ciphers_md.py to use the new combined images

--- generate_ciphers_md.py
+++ generate_ciphers_md.py
@@ -11,18 +11,12 @@ BASE_URL = "https://www.dcode.fr"
 BASE_PATH = pathlib.Path(__file__).parent.absolute()
 CIPHERS_PATH = "{}/ciphers".format(BASE_PATH)
 CIPHERS = sorted(next(os.walk(CIPHERS_PATH))[1])
-GITHUB_IMAGE_PATH_FORMAT = "./ciphers/{}/images/{}"
+GITHUB_IMAGE_PATH_FORMAT = "./ciphers/{}/combined.png"
 
 for cipher in CIPHERS:
     md_text += "\n### [{}]({}/{})\n".format(cipher, BASE_URL, cipher)
-    images = sorted(glob.glob("{}/{}/images/*.png".format(CIPHERS_PATH, cipher)))
-    for image in images:
-        filename = os.path.basename(image)
-        image_url = GITHUB_IMAGE_PATH_FORMAT.format(cipher, filename)
-        i = int(re.search(r'char\((.*?)\)', filename).group(1))
-        c = urllib.parse.quote(chr(i))
-        alt_text = "{} ({})".format(c, i)
-        md_text += "![{}]({}) ".format(alt_text, image_url)
+    image_url = GITHUB_IMAGE_PATH_FORMAT.format(cipher)
+    md_text += "![{}]({})".format(cipher, image_url)
 
 with open("CIPHERS.md", "w") as f:
     f.write(md_text)
