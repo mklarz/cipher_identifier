305326bf39ba5355257307ef9506200c4dd652ab
mklarz <mklarz@protonmail.com>
2020-10-29 14:45:20 +0100

    Handle the downloading of images better and change their local name to use the ASCII code

--- scripts/download_images.py
+++ scripts/download_images.py
@@ -2,8 +2,9 @@
 import os
 import re
 import cv2
-import requests
 import pathlib
+import requests
+import argparse
 import numpy as np
 
 BASE_PATH = "{}/ciphers".format(pathlib.Path(__file__).resolve().parents[1].absolute())
@@ -12,15 +13,37 @@ HOME_MESSAGE = "dCode</a> offers tools to win for sure, for example the <"
 
 def crop_image(img):
     # https://stackoverflow.com/a/49907762
+    # Modified to check borders
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = 255*(gray < 128).astype(np.uint8) # To invert the text to white
+    if not gray.any():
+        # Found no borders, return original image
+        return img
     coords = cv2.findNonZero(gray) # Find all non-zero points (text)
     x, y, w, h = cv2.boundingRect(coords) # Find minimum spanning bounding box
     rect = img[y:y+h, x:x+w] # Crop the image - note we do this on the original image
     return rect
 
-def download_cipher_images(cipher):
+def image_sizes_are_identical(images):
+    last_size = None
+    for image in images:
+        height, width, channels = image.shape
+        image_size = (height, width)
+        if last_size is not None and last_size != image_size:
+            return False
+        last_size = image_size
+    else:
+        return True
+
+def download_cipher_images(cipher, redownload=False):
     cipher_base_path = "{}/{}".format(BASE_PATH, cipher)
+
+    # Check if cipher exists
+    if not redownload and os.path.exists(cipher_base_path):
+        print("[WARNING] Cipher '{}' exists in {}, ignoring cipher.".format(cipher, BASE_PATH))
+        print("Use the redownload arugment (-r, --redownload) to force a redownload")
+        return
+
     cipher_images_path = "{}/images".format(cipher_base_path)
 
     # Find the images
@@ -53,28 +76,52 @@ def download_cipher_images(cipher):
     # Create directory if it doesn't exist
     os.makedirs(cipher_images_path, exist_ok=True)
 
+    raw_images = {}
     images_downloaded = set()
     for char in chars:
+        # i here is the ASCII number for the character
         i = int(char)
 
         # Don't download images twice
         if i in images_downloaded:
             continue
 
-        image_filename = "char({}).png".format(i)
+        image_filename = "{}.png".format(i)
+        image_urlname = "char({}).png".format(i)
         image_path = "{}/{}".format(cipher_images_path, image_filename)
-        image_url = "{}/{}".format(images_url, image_filename)
-        print("Fetching image: '{}' and saving it to {}".format(image_filename, cipher_images_path))
+        image_url = "{}/{}".format(images_url, image_urlname)
+        print("Fetching image: '{}' and saving it to {}".format(image_urlname, cipher_images_path))
         r = requests.get(image_url)
 
         if r.status_code != 200:
             print("[ERROR] Something went wrong when downloading image, status code: {}".format(r.status_code))
             exit(1)
 
-        # Read the image into CV2
-        nparr = np.frombuffer(r.content, np.uint8)
-        image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+        # Save the images for now, might need to overwrite them if they have borders
+        with open(image_path, "wb") as f:
+            f.write(r.content)
 
+        raw_images[image_path] = r.content
+        images_downloaded.add(i)
+
+    images = {}
+
+    # Read the images into CV2 objects
+    for image_path, image_bytes in raw_images.items():
+        # Read the image into CV2
+        nparr = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+        images[image_path] = image
+
+    # TODO: Handle this another way, we need to remove the white background for identical images as well
+    """
+    if not image_sizes_are_identical(images.values()):
+        print("Images are not unique, performing border/padding removal")
+        # Process the images if their sizes are NOT identical,
+        # meaning they most likely have padding (borders) added
+
+    """
+    for image_path, image in images.items():
         # Crop the image (remove borders i.e.)
         image = crop_image(image)
 
@@ -84,26 +131,67 @@ def download_cipher_images(cipher):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         exit(0)
-        """
-
+        """ 
         cv2.imwrite(image_path, image)
-
-        images_downloaded.add(i)
 
     print("Done!")
 
-"""
-# Download all from ciphers.txt
-with open("ciphers.txt") as f:
-    ciphers = f.read().splitlines()
-    for cipher in ciphers:
-        download_cipher_images(cipher)
-"""
+example_text = """Examples:
+ # Download images for a specific cipher (-c, --cipher)
+ download_images.py -c ancients-stargate-alphabet
+ # Download images for all (-a, --all) the images specified in the ciphers file
+ download_images.py -a
+ # Specify which ciphers file to use, needs to contain a list of the cipher names
+ download_images.py -a -cf ciphers.txt
+ # Force redownload of images for a cipher, even if they exist
+ download_images.py -r -c ancients-stargate-alphabet
+ # Force redownload of all ciphers and their images, even if they exist
+ download_images.py -r -a"""
 
-# TODO: use arguments instead?
-# Get info from input, example: ancients-stargate-alphabet
-print("Which cipher do you want to download images for?")
-print("Example: ancients-stargate-alphabet")
-print("From URL: https://www.dcode.fr/ancients-stargate-alphabet")
-cipher = input("> ")
-download_cipher_images(cipher)
+parser = argparse.ArgumentParser(
+    prog="download_images",
+    description="Download cipher images from dcode.fr",
+    epilog=example_text,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+parser.add_argument(
+    "-c", "--cipher",
+    help="the name of the cipher to download images from"
+)
+parser.add_argument(
+    "-cf", "--ciphers-file", default="ciphers.txt",
+    help="file containing the list of ciphers"
+)
+parser.add_argument(
+    "-a", "--all", default=False, const=True, nargs='?',
+    help="download all the ciphers specified in the ciphers file"
+)
+parser.add_argument(
+    "-r", "--redownload", default=False, const=True, nargs='?',
+    help="force redownload of ciphers and their images."
+)
+
+args = parser.parse_args()
+
+if not args.cipher and not args.all:
+    # Neither the cipher is specified and the --all parameter is False
+    print("[ERROR] Need to specify a cipher or use the --all parameter")
+    print(example_text)
+    exit(1)
+
+if args.cipher:
+    cipher = args.cipher
+    # Download images for a single cipher
+    download_cipher_images(cipher, redownload=args.redownload)
+else:
+    # Download images for all the ciphers
+    ciphers_file = args.ciphers_file
+    if not os.path.exists(ciphers_file):
+        print("[ERROR] Ciphers file does not exist:", ciphers_file)
+        exit(1)
+
+    with open(ciphers_file) as f:
+        ciphers = f.read().splitlines()
+        print("Loaded {} ciphers".format(len(ciphers)))
+        for cipher in ciphers:
+            download_cipher_images(cipher, redownload=args.redownload)
---
+++ scripts/generate_test_data.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python
+import os
+import math
+import glob
+import pathlib
+from PIL import Image, ImageDraw
+
+BASE_PATH = pathlib.Path(__file__).parent.absolute()
+CIPHERS_PATH = "{}/ciphers".format(BASE_PATH)
+CIPHERS = sorted(next(os.walk(CIPHERS_PATH))[1])
+
+#############################################################################
+
+
+def generate_combined_image(
+    image_paths,
+    images_per_row,
+    background_color,
+    padding_pixels,
+    add_initial_padding,
+    draw_grid,
+    grid_line_width,
+    grid_line_color,
+):
+    image_count = len(image_paths)
+    expected_row_count = math.ceil(image_count / images_per_row)
+
+    # Are all the image sizes identical? (and load the images into a list)
+    identical = True
+    images = []
+    last_size = None
+    for image_path in image_paths:
+        image = Image.open(image_path)
+        images.append(image)
+        if last_size is not None and last_size != image.size:
+            identical = False
+        last_size = image.size
+
+    if identical:
+        # Get the size (width, height) of the images (they are identical),
+        image_size = images[0].size
+        image_width = image_size[0]
+        image_final_width = image_width + (padding_pixels * 2)
+        image_height = image_size[1]
+        image_final_height = image_height + (padding_pixels * 2)
+
+        # Calculate the image width and handle the extra padding
+        combined_width = image_final_width * images_per_row
+        combined_width += (padding_pixels * 2) if add_initial_padding else 0
+
+        # Calculate the image height and handle the extra padding
+        combined_height = image_final_height * expected_row_count
+        combined_height += (padding_pixels * 2) if add_initial_padding else 0
+    else:
+        """
+        The sizes are different, we need to loop through the images
+        and calculate the width and height by checking each image per row.
+        """
+        x, y = 0, 0
+        rows = []
+        row_width = 0
+        row_height = 0
+        for image_index, image in enumerate(images):
+            x += image.size[0]
+
+            if image.size[1] > row_height:
+                # We have a image that's taller than another image in the row
+                row_height = image.size[1]
+
+            # Increase the row width
+            row_width += image.size[0]
+
+            if image_index > 0 and image_index % images_per_row == 0:
+                # New row, reset variables
+                x = 0
+                rows.append((row_width, row_height))
+                row_width = 0
+                row_height = 0
+
+        # Add the last row if missing
+        if expected_row_count != len(rows):
+            rows.append((row_width, row_height))
+
+        # Find widest row and add padding
+        combined_width = max([row[0] for row in rows])
+        combined_width += padding_pixels * ((images_per_row * 2) - 1)
+        combined_width += padding_pixels if add_initial_padding else 0
+
+        # Sum the height of the rows and add padding
+        combined_height = sum([row[1] for row in rows])
+        #combined_height += (padding_pixels * (len(rows) + (1 if add_initial_padding else 0)))
+        combined_height += padding_pixels * (len(rows) * 2)
+        combined_height += padding_pixels if add_initial_padding else 0
+
+    # Create the initial image with white background
+    combined = Image.new('RGB', (combined_width, combined_height), background_color)
+
+    # Initial positioning
+    reset_x = padding_pixels if add_initial_padding else 0
+    reset_y = padding_pixels if add_initial_padding else 0
+
+    reset_x += padding_pixels
+    reset_y += padding_pixels
+
+    x, y = reset_x, reset_y
+
+    x_end = combined.size[0] - (padding_pixels if add_initial_padding else 0)
+    y_end = combined.size[1] - (padding_pixels if add_initial_padding else 0)
+
+    # Loop through all the images and add them to the combined image
+    row_index = 0
+
+
+    for i, image in enumerate(images):
+        image_index = i + 1
+        combined.paste(image, (x, y), mask=image.convert("RGBA"))
+
+        # New row
+        if identical:
+            row_height = image_final_height
+        else:
+            row_height = rows[row_index][1] + (padding_pixels * 2)
+
+        is_last_row = (row_index + 1) == expected_row_count
+        is_new_row = image_index != 1 and image_index % images_per_row == 0
+        is_last_image = image_index == image_count
+        is_last_image_of_row = image_index % images_per_row == 0
+
+        # Add vertical grid line?
+        if draw_grid and x != reset_x:
+            # Find the x position between the rows and add a line there
+            line_x = x - padding_pixels - (grid_line_width / 2)
+            line_y_end = row_height
+
+            if row_index == 0 or is_last_row:
+                if is_last_row:
+                    line_y_start = y - padding_pixels
+                    line_y_end = y_end
+                else:
+                    line_y_start = padding_pixels if add_initial_padding else 0
+                    line_y_end += padding_pixels if add_initial_padding else 0
+            else:
+                line_y_start = y - padding_pixels
+                line_y_end += line_y_start
+
+            draw = ImageDraw.Draw(combined)
+            draw.rectangle([
+                    (line_x, line_y_start),
+                    (line_x + grid_line_width, line_y_end)
+                ],
+                fill=grid_line_color
+            )
+
+        """
+        # Add the last vertical line to each row?
+        if is_last_image or is_last_image_of_row:
+            # Add the last vertical line
+            line_x += image_final_width if identical else (image.size[0] + (padding_pixels * 2))
+            draw = ImageDraw.Draw(combined)
+            draw.rectangle([
+                    (line_x, line_y_start),
+                    (line_x + grid_line_width, line_y_end)
+                ],
+                fill=grid_line_color
+            )
+        """
+
+        if not is_new_row:
+            # Just increase the current x position
+            x += image_final_width if identical else (image.size[0] + (padding_pixels * 2))
+        else:
+            # New row
+            x = reset_x
+            y += row_height
+
+            # Add horizontal grid line?
+            if draw_grid and not is_last_row:
+                # Find the y position between the rows and add a line there
+                line_x = padding_pixels if add_initial_padding else 0
+                line_y = y - padding_pixels - (grid_line_width / 2)
+                draw = ImageDraw.Draw(combined)
+                draw.rectangle([
+                        (line_x, line_y),
+                        (x_end, line_y + grid_line_width)
+                    ],
+                    fill=grid_line_color
+                )
+
+            row_index += 1
+
+    return combined
+
+
+for cipher in CIPHERS:
+    print("Generating combined images for cipher:", cipher)
+    cipher_path = "{}/{}".format(CIPHERS_PATH, cipher)
+    cipher_images_path = "{}/images".format(cipher_path)
+    image_paths = sorted(glob.glob("{}/*.png".format(cipher_images_path)))
+
+    for draw_grid in (True, False):
+        combined_image = generate_combined_image(
+            image_paths=image_paths,
+            images_per_row=IMAGES_PER_ROW,
+            background_color=BACKGROUND_COLOR,
+            padding_pixels=PADDING_PIXELS,
+            add_initial_padding=INITIAL_PADDING,
+            draw_grid=draw_grid,
+            grid_line_width=GRID_LINE_WIDTH,
+            grid_line_color=GRID_LINE_COLOR,
+        )
+        combined_path = "{}/combined{}.png".format(cipher_path, "_grid" if draw_grid else "")
+        combined_image.save(combined_path, "PNG")
+
