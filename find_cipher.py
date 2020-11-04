89e55c416880b11560a20afeef753a898751e807
mklarz <mklarz@protonmail.com>
2020-11-04 23:19:19 +0100

    Add initial trained tesseract data for ancients-stargate-alphabet

--- find_cipher.py
+++ find_cipher.py
@@ -1,100 +1,79 @@
-#!/usr/bin/python
 import os
 import sys
-import cv2
-import glob
 import time
 import pathlib
-import numpy as np
-
-BASE_PATH = "{}/ciphers".format(pathlib.Path(__file__).parent.absolute())
+import tesserocr
+from PIL import Image
+from tesserocr import PyTessBaseAPI, RIL, PSM, OEM
 
 if len(sys.argv) != 2:
     print("[ERROR] Invalid amount or no arguments")
     print("Example usage: python find_cipher.py image.png")
     exit(1)
 
-image_path = sys.argv[1] 
+image_path = sys.argv[1]
 
 if not os.path.exists(image_path):
     print("[ERROR] Could not find image:", image_path)
     exit(1)
 
-IMG = cv2.imread(image_path)
+BASE_PATH = pathlib.Path(__file__).resolve().parent.absolute()
+# TESSDATA_PATH = "/usr/share/tessdata"
+TESSDATA_PATH = "{}/models/tessdata".format(BASE_PATH)
+CIPHER_LANGUAGES = sorted([f.replace(".traineddata", "") for f in os.listdir(TESSDATA_PATH) if "traineddata" in f])
 
-ciphers = sorted(next(os.walk(BASE_PATH))[1])
+"""
+PSM:
+Page segmentation modes.
 
-# From https://stackoverflow.com/a/15147009 and modified
-# Doc: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_template_matching/py_template_matching.html?highlight=minmaxloc
-def get_minimum_value(image):
-    method = cv2.TM_SQDIFF_NORMED
-    small_image = cv2.imread(image)
-    large_image = IMG
+ 0  OSD_ONLY   Orientation and script detection (OSD) only.
+ 1  AUTO_OSD   Automatic page segmentation with OSD.
+ 2  AUTO_ONLY  Automatic page segmentation, but no OSD, or OCR.
+ 3  AUTO       Fully automatic page segmentation, but no OSD. (Default)
+ 4  SINGLE_COLUMN  Assume a single column of text of variable sizes.
+ 5  SINGLE_BLOCK_VERT_TEXT  Assume a single uniform block of vertically aligned text.
+ 6  SINGLE_COLUMN  Assume a single uniform block of text.
+ 7  SINGLE_LINE    Treat the image as a single text line.
+ 8  SINGLE_WORD    Treat the image as a single word.
+ 9  CIRCLE_WORD    Treat the image as a single word in a circle.
+ 10  SINGLE_CHAR   Treat the image as a single character.
+ 11  SPARSE_TEXT      Sparse text. Find as much text as possible in no particular order.
+ 12  SPARSE_TEXT_OSD  Sparse text with OSD.
+ 13  RAW_LINE         Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
+"""
+PSM_MODE = PSM.SINGLE_LINE
 
-    """
-    Template Matching is a method for searching and finding the
-    location of a template image in a larger image.
-    OpenCV comes with a function cv2.matchTemplate() for this purpose.
-    It simply slides the template image over the
-    input image (as in 2D convolution) and compares the template
-    and patch of input image under the template image. 
-    Several comparison methods are implemented in OpenCV.
-    (You can check docs for more details). 
-    It returns a grayscale image, where each pixel denotes 
-    how much does the neighbourhood of that pixel match with template.
-    """
-    result = cv2.matchTemplate(small_image, large_image, method)
+image = Image.open(image_path)
 
-    """
-    If input image is of size (WxH) and template image is of size (wxh),
-    output image will have a size of (W-w+1, H-h+1).
-    Once you got the result, you can use cv2.minMaxLoc() function to fin
-    where is the maximum/minimum value. 
-    Take it as the top-left corner of rectangle and take (w,h) as width
-    and height of the rectangle. That rectangle is your region of template.
-    """
-    min_value,_,_,_ = cv2.minMaxLoc(result)
-
-    return min_value
-
-def calculate_weight(values):
-    # TODO: tweak this to use an actual weight algo?
-    weight = round(sum(values) / len(values), 3)
-    return 0 if weight == 1 else weight
-
-cipher_weights = {}
-for cipher in ciphers:
+cipher_results = {}
+for cipher_language in CIPHER_LANGUAGES:
     start = time.process_time()
-    print("Checking:", cipher, end=" = ")
-    cipher_path = "{}/{}".format(BASE_PATH, cipher)
-    images_path = "{}/images".format(cipher_path)
-    images = glob.glob("{}/*.png".format(images_path))
-    image_count = len(images)
+    print("Checking:", cipher_language, end=" = ")
 
-    values = []
-    for index, filename in enumerate(images):
-        values.append(get_minimum_value(filename))
+    with PyTessBaseAPI(path=TESSDATA_PATH, lang=cipher_language, psm=PSM_MODE) as api:
+        api.SetImage(image)
+        text = api.GetUTF8Text().strip()
+        # TODO: Get confidence for each character / word?
+        word_count = len(api.AllWordConfidences())
+        confidence = api.MeanTextConf()
+        print(confidence, end="")
 
-    weight = calculate_weight(values)
-    print(weight, end="")
-    cipher_weights[cipher] = weight
+    cipher_results[cipher_language] = {
+        "text": text,
+        "confidence": confidence,
+        "word_count": word_count,
+    }
 
     end = time.process_time() - start
     print(", took {} seconds".format(end))
 
-# Sort the by weight
-cipher_weights = {k: v for k, v in sorted(cipher_weights.items(), reverse=True, key=lambda item: item[1])}
+# Sort the by confidence
+cipher_results = {k: v for k, v in sorted(cipher_results.items(), reverse=True, key=lambda item: item[1]["confidence"])}
 
 print("=" * 32)
 print("Top ciphers:")
-for index, cipher in enumerate(cipher_weights):
-    position = index + 1
-    weight = cipher_weights[cipher]
-
-    if weight == 0:
-        # Not a match
-        continue
-
-    print(position, weight, cipher, sep="\t")
+for index, cipher in enumerate(cipher_results):
+    result = cipher_results[cipher]
+    print(index + 1, "{}%".format(result["confidence"]), cipher, result["text"], sep="\t")
 
 print("=" * 32)
