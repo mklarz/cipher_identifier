c2239ded5ef2e1741909c0bbc3194701a8b58adb
mklarz <mklarz@protonmail.com>
2020-11-05 13:05:35 +0100

    Update CIPHERS.md with new ciphers and URL

--- scripts/generate_ciphers_md.py
+++ scripts/generate_ciphers_md.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 import glob
 import os
+import json
 import pathlib
 import re
 import urllib.parse
 
 md_text = "# Cipher list\n"
 
-BASE_URL = "https://www.dcode.fr"
 BASE_PATH = pathlib.Path(__file__).resolve().parents[1].absolute()
 CIPHERS_PATH = "{}/ciphers".format(BASE_PATH)
 CIPHERS = sorted(next(os.walk(CIPHERS_PATH))[1])
-GITHUB_IMAGE_PATH_FORMAT = "./ciphers/{}/combined.png"
+GITHUB_CIPHER_PATH_FORMAT = "./ciphers/{}"
 
 for cipher in CIPHERS:
-    md_text += "\n### [{}]({}/{})\n".format(cipher, BASE_URL, cipher)
-    image_url = GITHUB_IMAGE_PATH_FORMAT.format(cipher)
+    cipher_path = "./ciphers/{}".format(cipher)
+    with open("{}/cipher.json".format(cipher_path)) as f:
+        cipher_title = json.load(f)["title"]
+
+    md_text += "\n### [{}]({})\n".format(cipher_title, cipher_path)
+    image_url = "{}/combined.png".format(cipher_path)
     md_text += "![{}]({})".format(cipher, image_url)
 
 with open("CIPHERS.md", "w") as f:
